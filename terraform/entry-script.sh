eea01d6bc8fec7f5378127589523d7e2cfc51b6d
root <root@localhost.localdomain>
2023-07-26 09:59:21 +0300

    last

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -3,4 +3,7 @@ sudo yum update -y
 sudo yum install -y docker
 sudo systemctl start docker
 sudo usermod -aG docker ec2-usermod
-docker run -p 8080:80 docker.io/mahmoudabdelgowad/my-images:3.0
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo curl https://raw.githubusercontent.com/mabdelgowa/demo-/main/Dockercompose.yaml -o Dockercompose.yaml
+sudo docker compose -f Dockercompose.yaml up
