c1d41fe74ca75818f9c6a4ce0afa0a0fb2ae6841
root <root@localhost.localdomain>
2023-07-26 10:38:49 +0300

    compose

--- terraform/entry-script.sh
+++ terraform/entry-script.sh
@@ -6,4 +6,4 @@ sudo usermod -aG docker ec2-usermod
 sudo curl -SL https://github.com/docker/compose/releases/download/v2.20.2/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo curl https://raw.githubusercontent.com/mabdelgowa/demo-/main/Dockercompose.yaml -o Dockercompose.yaml
-sudo docker compose -f Dockercompose.yaml up
+sudo docker-compose -f Dockercompose.yaml up
