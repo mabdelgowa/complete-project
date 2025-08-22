a1fb072d5be810e70a9830c3525a270657cde5ed
Mahmoud Abdelgawad <55905528+mabdelgowa@users.noreply.github.com>
2025-08-23 01:10:24 +0300

    Update db.go

--- db.go
+++ db.go
@@ -1,83 +1,58 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-	"strconv"
-	"time"
+	"database/sql"
+	"fmt"
+	"os"
+
+	mysqldriver "github.com/go-sql-driver/mysql"
 )
 
-func main() {
-	// db.go runs init() automatically, so _connection is ready
-	http.ListenAndServe(":9090", &handler{})
-}
+var _connection *sql.DB
 
-type handler struct{}
-
-type row struct {
-	ID        int64     `json:"id"`
-	CreatedAt time.Time `json:"created_at"`
-}
-
-func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path == "/healthcheck" {
-		w.WriteHeader(200)
-		w.Write([]byte(`{"status":"ok"}`))
-		return
+func init() {
+	mysqlConfig := &mysqldriver.Config{
+		User:                 os.Getenv("MYSQL_USER"),
+		Passwd:               os.Getenv("MYSQL_PASS"),
+		Net:                  "tcp",
+		Addr:                 fmt.Sprintf("%s:%s", os.Getenv("MYSQL_HOST"), os.Getenv("MYSQL_PORT")),
+		AllowNativePasswords: true,
+		ParseTime:            true,
 	}
 
-	switch r.Method {
-	case "GET":
-		rs, err := _connection.Query(`SELECT id,created_at FROM stuff`)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-		defer rs.Close()
+	connection, err := sql.Open("mysql", mysqlConfig.FormatDSN())
+	if err != nil {
+		panic(err)
+	}
 
-		var ret []row
-		for rs.Next() {
-			cur := row{}
-			err = rs.Scan(
-				&cur.ID,
-				&cur.CreatedAt,
-			)
-			if err != nil {
-				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-				return
-			}
-			ret = append(ret, cur)
-		}
+	_, err = connection.Exec(`CREATE DATABASE IF NOT EXISTS internship`)
+	if err != nil {
+		panic(err)
+	}
 
-		bs, err := json.Marshal(ret)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-		w.Write(bs)
+	mysqlConfig.DBName = `internship`
 
-	case "POST":
-		_, err := _connection.Exec("INSERT INTO stuff (created_at) VALUES (NOW())")
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
-		w.Write([]byte(`OK`))
+	if err = connection.Close(); err != nil {
+		panic(err)
+	}
 
-	case "PATCH":
-		idStr := r.URL.Query().Get("id")
-		id, err := strconv.ParseInt(idStr, 10, 64)
-		if err != nil {
-			http.Error(w, "Invalid id value", http.StatusBadRequest)
-			return
-		}
+	connection, err = sql.Open("mysql", mysqlConfig.FormatDSN())
+	if err != nil {
+		panic(err)
+	}
 
-		_, err = _connection.Exec(`UPDATE stuff SET created_at=NOW() WHERE id=?`, id)
-		if err != nil {
-			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-			return
-		}
+	_connection = connection
 
-		w.Write([]byte(`OK`))
+	schema()
+}
+
+func schema() {
+	_, err := _connection.Exec(`CREATE TABLE IF NOT EXISTS stuff (
+		id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		created_at DATETIME NOT NULL
+	)`)
+
+	if err != nil {
+		panic(err)
 	}
 }
